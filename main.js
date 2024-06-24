6ef1624558d795cbd671ca883b0cd6d91d293fc0
gabrielaspooky <gabrielanava01@gmail.com>
2024-06-24 20:30:51 +0200

    added new elements

---
+++ main.js
@@ -0,0 +1,9 @@
+function addHTMLTags() {
+
+    const mainTitle = document.createElement("h1");
+    const taskInput = document.createElement("input")
+    const addButton = document.createElement("button");
+    const clearButton = document.createElement("button");
+
+
+}
